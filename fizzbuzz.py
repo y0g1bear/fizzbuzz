558eed6331966454b2f9d4d7e685f3a7b2a3fcc3
Eric Dennison <ericd@netdenizen.com>
2015-09-15 21:49:45 -0400

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,19 @@
-Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+"""
+fizzbuzz.py
+Author: <your name here>
+Credit: <list sources used, if any>
 
-We will use a variation of this test in which the last number of the series isn't necessarily 100, and the two numbers being tested for multiples aren't necessarily three and five. For example, your program should behave just like this:
+Assignment:
+
+Write a program that prints the numbers from 1 to 100. But for 
+multiples of three print “Fizz” instead of the number and for 
+the multiples of five print “Buzz”. For numbers which are multiples 
+of both three and five print “FizzBuzz”.
+
+We will use a variation of this test in which the last number of 
+the series isn't necessarily 100, and the two numbers being tested 
+for multiples aren't necessarily three and five. For example, your 
+program should behave just like this:
 
 How many numbers shall we print? 25
 For multiples of what number shall we print 'Fizz'? 3
@@ -30,3 +43,4 @@ Fizz
 23
 Fizz
 Buzz
+"""
